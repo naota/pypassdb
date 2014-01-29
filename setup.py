6dd9e4a8755ca804f409c076752f3b7ad4b55642
Naohiro Aota <naota@gentoo.org>
2014-01-29 15:59:45 +0900

    Add py.test runner

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
 setup(name='pypassdb',
       version='0.1.0',
       author='Naohiro Aota',
       author_email='naota@elisp.net',
       url='https://github.com/naota/pypassdb',
-      packages=['pypassdb']
+      packages=['pypassdb'],
+      cmdclass = {'test': PyTest},
       )
