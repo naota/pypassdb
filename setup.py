5165a0244b07ea1e9f5b7fb313e27b6fd01e00fc
Naohiro Aota <naota@gentoo.org>
2014-01-29 15:50:22 +0900

    Add package setup files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pypassdb',
+      version='0.1.0',
+      author='Naohiro Aota',
+      author_email='naota@elisp.net',
+      url='https://github.com/naota/pypassdb',
+      packages=['pypassdb']
+      )
