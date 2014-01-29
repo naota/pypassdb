d0579c8338e3fd7d5af9b0e256cefb73a0438884
Naohiro Aota <naota@gentoo.org>
2014-01-29 16:19:28 +0900

    setup.py: Cosmetic change
    Supress style warnings.

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from distutils.core import setup, Command
+
+
 class PyTest(Command):
+
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        import sys,subprocess
+        import sys
+        import subprocess
         errno = subprocess.call([sys.executable, 'runtests.py'])
         raise SystemExit(errno)
+
 setup(name='pypassdb',
       version='0.1.0',
       author='Naohiro Aota',
       author_email='naota@elisp.net',
       url='https://github.com/naota/pypassdb',
       packages=['pypassdb'],
-      cmdclass = {'test': PyTest},
+      cmdclass={'test': PyTest},
       )
